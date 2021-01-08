73b4e071fb79aeaf931c67dc5dde496eb5a2a213
johsnows <sdut_snow@163.com>
2021-01-08 17:05:02 +0800

    update

--- find_models.py
+++ find_models.py
@@ -38,7 +38,7 @@ if cuda_available:
 else:
     print('Using CPU.')
 # ofa_network = ofa_net('ofa_mbv3_d234_e346_k357_w1.2', pretrained=True)
-ofa_network = ofa_net('ofa_resnet', pretrained=True)
+ofa_network = ofa_net('ofa_resnet50', pretrained=True)
 print('The OFA Network is ready.')
 if cuda_available:
     # path to the ImageNet dataset
